3a591c1627d3426b21194ce0c24ba7ab25afcdce
jmdaku <jmdaku@gmail.com>
2023-12-10 18:19:50 -0500

    added general packages and user-related classes; started database connection

---
+++ src/main/java/org/launchcode/parksfortails/ParksForTailsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.parksfortails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParksForTailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksForTailsApplication.class, args);
+	}
+
+}
