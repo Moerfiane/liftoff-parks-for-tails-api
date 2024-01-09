8a92125607b1e7db5b4f5384bab0363c85d29269
jmdaku <jmdaku@gmail.com>
2024-01-08 21:37:14 -0500

    restarted to fix gradle

---
+++ src/main/java/com/launchcode/parksfortails/controllers/UserController.java
@@ -0,0 +1,133 @@
+package com.launchcode.parksfortails.controllers;
+
+import com.launchcode.parksfortails.data.UserRepository;
+import com.launchcode.parksfortails.models.User;
+import com.launchcode.parksfortails.models.dto.RegistrationFormDTO;
+import com.launchcode.parksfortails.models.dto.LoginFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.Optional;
+import org.springframework.validation.Errors;
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";  // user ID key
+
+    //stores user session
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+    //get user if session exists (not null or empty)
+    public User getUserFromSession(HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> userOpt = userRepository.findById(userId);
+
+        if (userOpt.isEmpty()) {
+            return null;
+        }
+
+        return userOpt.get();
+    }
+
+    //registration form
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model, HttpSession session) {
+        model.addAttribute(new RegistrationFormDTO());
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(@ModelAttribute @Validated RegistrationFormDTO registrationFormDTO,
+                                          Errors errors,
+                                          HttpServletRequest request) {
+
+        if (errors.hasErrors()) {
+            return "register";
+        }
+
+        User existingUser = userRepository.findByUsername(registrationFormDTO.getUsername());
+
+        if (existingUser != null) {
+            errors.rejectValue("username", "username.alreadyExists", "That username already exists");
+            return "register";
+        }
+
+        //checking if password and verify password fields match
+        String password = registrationFormDTO.getPassword();
+        String verifyPassword = registrationFormDTO.getVerifyPassword();
+        if (!password.equals(verifyPassword)) {
+            errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
+            return "register";
+        }
+
+        //saves user & password; logs in & sends to search page
+        User newUser = new User(registrationFormDTO.getUsername(), registrationFormDTO.getPassword());
+        userRepository.save(newUser);
+        setUserInSession(request.getSession(), newUser);
+        return "redirect:/search";
+    }
+
+    //login page
+    @GetMapping("/login")
+    public String displayLoginForm(Model model, HttpSession session) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("loggedIn", session.getAttribute("user") != null);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Validated LoginFormDTO loginFormDTO,
+                                   Errors errors,
+                                   HttpServletRequest request) {
+
+        if (errors.hasErrors()) {
+            return "login";
+        }
+
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        String password = loginFormDTO.getPassword();
+
+        //verify if username exists and username + password (hashed) match
+        if (theUser == null || !theUser.isMatchingPassword(password)) {
+            errors.rejectValue(
+                    "password",
+                    "login.invalid",
+                    "Invalid login. Please try again."
+            );
+            return "login";
+        }
+
+        //if correct, login & send to search page
+        setUserInSession(request.getSession(), theUser);
+        return "redirect:/search";
+    }
+
+    //logout page
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+
+    }
+}
---
+++ src/main/java/com/launchcode/parksfortails/models/User.java
@@ -0,0 +1,85 @@
+package com.launchcode.parksfortails.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Objects;
+
+
+//@MappedSuperclass
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 5, max = 20, message= "Username must be between 5 & 20 characters.")
+    private String username;
+
+    @Email(message = "Must be a valid email.")
+    private String email;
+
+    @NotNull
+    private String pwhash;
+
+    public User() {}
+
+    //does id & email need to be here?
+    public User(String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.pwhash = encoder.encode(password);
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwhash);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(email, user.email);
+    }
+
+    // may only need to match id?
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email);
+    }
+}
